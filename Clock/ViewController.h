c4219dfa6a1815ffdcb48c09d0c0a7bbda7edd65
morobororo <e115747@ie.u-ryukyu.ac.jp>
2013-01-26 15:02:15 +0900

    Initial Commit

---
+++ Clock/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Clock
+//
+//  Created by Miwa Oshiro on 2013/01/26.
+//  Copyright (c) 2013年 edu.stanford.cs193p.oshiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
