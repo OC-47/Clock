76700c1bed239d9fe573bdda79f334c6f59ba264
morobororo <e115747@ie.u-ryukyu.ac.jp>
2013-02-11 18:55:28 +0900

    公開

---
+++ Clock/hogeeeeeeeeee.h
@@ -0,0 +1,13 @@
+//
+//  hogeeeeeeeeee.h
+//  Clock
+//
+//  Created by Miwa Oshiro on 2013/02/09.
+//  Copyright (c) 2013年 edu.stanford.cs193p.oshiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface hogeeeeeeeeee : UITableViewController
+
+@end
